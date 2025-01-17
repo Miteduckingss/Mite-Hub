ce96fae669c2cf59e4277ad1318e62fb7f72b52f
Miteduckingss <123892340+Miteduckingss@users.noreply.github.com>
2025-01-18 05:54:38 +0700

    Update Loader.lua

--- Utilities/AX/Loader.lua
+++ Utilities/AX/Loader.lua
@@ -1,7 +1,7 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/refs/heads/main/Utilities/AX/AutoKick.lua"), true))()
 local Games = {
-  [286090429] = "https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/main/Games/Key/Arsenal.lua",
-  [1537690962] = "https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/main/Games/Key/BeeSwarmSimulator.lua"
+  [1537690962] = "https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/refs/heads/main/Utilities/AX/Games/Lebah",
+  [16732694052] = "https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/refs/heads/main/Utilities/AX/Games/Ikan"
 }
 if Games[game.PlaceId] then 
   loadstring(game:HttpGet(Games[game.PlaceId]))()
