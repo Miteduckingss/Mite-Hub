a93e6c2211683153e9e7d18f79f8456cb32e0e2f
Miteduckingss <123892340+Miteduckingss@users.noreply.github.com>
2025-01-18 05:51:53 +0700

    Update Loader.lua

--- Utilities/AX/Loader.lua
+++ Utilities/AX/Loader.lua
@@ -1,12 +1,10 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/refs/heads/main/Utilities/AX/AutoKick.lua"), true))()
 local Games = {
   [286090429] = "https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/main/Games/Key/Arsenal.lua",
-  [7239319209] = "https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/main/Games/Key/Ohio.lua",
-  [189707] = "https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/main/Games/Keyless/NaturalDisasterSurvival.lua",
-  [3696971654] = "https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/main/Games/Keyless/NaturalDisasterSurvival.lua",
   [1537690962] = "https://raw.githubusercontent.com/Miteduckingss/Mite-Hub/main/Games/Key/BeeSwarmSimulator.lua"
 }
 if Games[game.PlaceId] then 
   loadstring(game:HttpGet(Games[game.PlaceId]))()
  else
- game.Players.LocalPlayer:Kick("Games Not Supported")
+ print("oh no")
 end
